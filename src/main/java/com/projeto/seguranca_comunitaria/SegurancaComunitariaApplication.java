cdab02136eb7162d787a88e15c0b53a007d33c8b
Ana Luiza Bastos <aninhabastos98@hotmail.com>
2026-05-21 11:23:31 -0300

    Project created

---
+++ src/main/java/com/projeto/seguranca_comunitaria/SegurancaComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.seguranca_comunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancaComunitariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/seguranca_comunitaria/SegurancaComunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.seguranca_comunitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaComunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
